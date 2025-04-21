00d9ece97ec633d44dc1ca9c3180ceb919983c3a
Jasper Eldrich Ballesteros <jeballesteros@ASI.ALLIANCE.COM.PH>
2025-04-21 13:39:42 +0800

    make token expires in 1 hour

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -42,7 +42,7 @@ export const signIn = async (req, res) => {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'Lax',
-        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        maxAge: 60 * 60 * 1000, // 7 days
       });
   
       res.status(200).json({ success: true, user: data.user });
