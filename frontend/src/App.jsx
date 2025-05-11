631fb6c9a51f6bd75300509077fb4072e26504ff
Perod122 <jasperballesteros21@gmail.com>
2025-05-11 23:18:03 +0800

    vercel deployment 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ import AuthRedirect from "./components/AuthRedirect";
 import LoadingScreen from "./components/LoadingScreen";
 
 // Pages
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/loginPage";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
